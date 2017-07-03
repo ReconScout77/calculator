5029ab3d53836bdc5f35526293cd7fe79608328a
Andrew Parks and Kaili Nishihira <andrew@>
2017-07-03 15:09:34 -0700

    Add F to C temperture calculator.

---
+++ js/temp.js
@@ -0,0 +1,7 @@
+var fahrenheit = parseInt(prompt("What is the temperature in Celsius?")) * 1.8 + 32;
+
+alert("The temperature is " + fahrenheit + " degrees F");
+
+var celsius = parseInt(prompt("What is temperature in Fahrenheit")) - 32 * 5 / 9;
+
+alert("The temperature is " + celsius  + " degrees C");
