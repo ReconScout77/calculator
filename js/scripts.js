f8222609aeaef9256e3b6c971cb2e3f1d8aa7bd1
Kaili Nishihira and Robert Murray <robert@>
2017-07-05 15:58:40 -0700

    Add user interface in JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,9 +16,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// User interface (or front-end) logic:
 
-var result = divide(number1, number2);
-
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
